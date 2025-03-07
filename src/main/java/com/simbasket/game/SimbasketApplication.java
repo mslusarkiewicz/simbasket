06c25e0d9d80189ba13ab3aa5d6e9f1735fa6494
mslusarkiewicz <mslusarkiewicz@yahoo.pl>
2025-03-07 14:48:38 +0100

    init

---
+++ src/main/java/com/simbasket/game/SimbasketApplication.java
@@ -0,0 +1,13 @@
+package com.simbasket.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simbasket/game/SimbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbasket.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
